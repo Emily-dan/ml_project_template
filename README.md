674a851ef641ac6aee0d07b883644a3d4a1f58de
Emily <emily921220@163.com>
2021-03-29 14:37:08 +0800

    binary regression metrics

--- README.md
+++ README.md
@@ -22,15 +22,15 @@ Code by Abhishek Thakur's video
 ## Binary Classification Metrics
 
 - Accuracy: $\frac{TP+TN}{TP+TN+FP+FN}$
-- Precision: `$\frac{TP}{TP+FP}$`
-- Recall: `$\frac{TP}{TP+FN}$`
-- F1-score(F1): `$\frac{2RP}{R+P}=\frac{2TP}{2TP+FP+FN}$`
+- Precision: $\frac{TP}{TP+FP}$
+- Recall: $\frac{TP}{TP+FN}$
+- F1-score(F1): $\frac{2RP}{R+P}=\frac{2TP}{2TP+FP+FN}$
 - AUC(Area under the ROC Curve):
   - ROC:Receiver Operating Characteristics
-  - TPR(True Positive Rate): `$\frac{TP}{TP+FN}$`
-  - FPR(False positive rate): `$\frac{FP}{TN+FP}$`
+  - TPR(True Positive Rate): $\frac{TP}{TP+FN}$
+  - FPR(False positive rate): $\frac{FP}{TN+FP}$
   - [introduction](https://blog.csdn.net/u013385925/article/details/80385873)
-- logloss: `$y -(\log(p) + (1-y) \log(1-p))$`
+- logloss: $y -(\log(p) + (1-y) \log(1-p))$
 
 ## Encoding
 
@@ -38,3 +38,17 @@ Code by Abhishek Thakur's video
 - label encoding
 - one hot encoding
 - embedding
+
+## Binary Regression Metrics
+
+- AE(absolute error): $\abs(y_{true} - y_{pred})$
+- MAE(mean absolute error): $\frac{1}{N} \sum_{i=1}^N \abs(y_{true_i} - y_{pred_i})$
+- SE(squared error): $(y_{true} - y_{pred})^2$
+- MSE(mean squared error): $\frac{1}{N} \sum_{i=1}^N (y_{true_i} - y_{pred_i})^2$
+- SLE(squared log error): $(\log(1+y_{true}) - \log(1+y_{pred}))^2$
+- MSLE(mean squared log error): $\frac{1}{N} \sum_{i=1}^N (\log(1+y_{true_i}) - \log(1+y_{pred_i}))^2$
+- RMSE(root MSE): $\sqrt{MSE}$
+- RMSLE(root MSLE): $\sqrt{MSLE}$
+- $R^2$ (coefficient of determination): 
+  - $R^2 = 1 - \frac{\sum_{i=1}^N (y_{true_i} - y_{pred_i})^2}{\sum_{i=1}^N (y_{true_i} - mean)^2}$
+  - $mean = \frac{i}{N} \sum_{i=1}^N y_{true_i}$
--- src/metrix.py
+++ src/metrix.py
@@ -1,4 +1,45 @@
+import numpy as np
 from sklearn import metrics as skmetrics
+
+
+class RegressionMetrics:
+    def __init__(self):
+        self.metrics = {
+            "mae": self._mae,
+            "mse": self._mse,
+            "rmse": self._rmse,
+            "msle": self._msle,
+            "rmsle": self._rmsle,
+            "r2": self._r2
+        }
+
+    def __call__(self, metric, y_true, y_pred):
+        if metric not in self.metrics:
+            raise Exception("Metric not implemented!")
+        else:
+            return self.metrics[metric](y_true, y_pred)
+
+    @staticmethod
+    def _mae(y_true, y_pred):
+        return skmetrics.mean_absolute_error(y_true, y_pred)
+
+    @staticmethod
+    def _mse(y_true, y_pred):
+        return skmetrics.mean_squared_error(y_true, y_pred)
+
+    def _rmse(self, y_true, y_pred):
+        return np.sqrt(self._mse(y_true, y_pred))
+
+    @staticmethod
+    def _msle(y_true, y_pred):
+        return skmetrics.mean_squared_log_error(y_true, y_pred)
+
+    def _rmsle(self, y_true, y_pred):
+        return np.sqrt(self._msle(y_true, y_pred))
+
+    @staticmethod
+    def _r2(y_true, y_pred):
+        return skmetrics.r2_score(y_true, y_pred)
 
 
 class ClassificationMetrics:
@@ -54,9 +95,19 @@ class ClassificationMetrics:
 
 
 if __name__ == "__main__":
-    cm = ClassificationMetrics()
+    # cm = ClassificationMetrics()
+    # t = [0, 1, 0, 0, 1, 0]
+    # p = [0, 1, 0, 0, 0, 0]
+    # print(cm("accuracy", t, p))
+    # print(cm("auc", t, p, p))
+    # print(cm("logloss", t, p, t))
+
+    rm = RegressionMetrics()
     t = [0, 1, 0, 0, 1, 0]
-    p = [0, 1, 0, 0, 0, 0]
-    print(cm("accuracy", t, p))
-    print(cm("auc", t, p, p))
-    print(cm("logloss", t, p, t))
+    p = [0.1, 0.8, 0, 0.2, 0.9, 0]
+    print(rm("mae", t, p))
+    print(rm("mse", t, p))
+    print(rm("rmse", t, p))
+    print(rm("msle", t, p))
+    print(rm("rmsle", t, p))
+    print(rm("r2", t, p))
